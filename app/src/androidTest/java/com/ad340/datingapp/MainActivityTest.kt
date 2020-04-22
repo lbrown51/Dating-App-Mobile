7177437c2e3edb67c3e3367c07f0cdaaddd6c3d5
Lenny Brown <penn2014@gmail.com>
2020-04-22 14:33:24 -0700

    Added all tests and create bundle for profile activity.

--- app/src/androidTest/java/com/ad340/datingapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/ad340/datingapp/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.ad340.datingapp
 
-
+import android.view.View
+import android.widget.DatePicker
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -8,11 +9,15 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.contrib.PickerActions
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -20,7 +25,67 @@ class MainActivityTest {
 
     @Test
     fun hasTextOnScreen() {
-        Espresso.onView(withId(R.id.hello_text))
-            .check(ViewAssertions.matches(withText(R.string.hello_world)))
+        onView(withId(R.id.hello_text))
+            .check(matches(withText(R.string.hello_world)))
+    }
+
+    @Test
+    fun checkHintMatches() {
+        onView(withId(R.id.name_edit_text))
+            .check(matches(withHint(R.string.enter_name)))
+
+        onView(withId(R.id.email_edit_text))
+            .check(matches(withHint(R.string.enter_email)))
+
+        onView(withId(R.id.username_edit_text))
+            .check(matches(withHint(R.string.enter_username)))
+
+        onView(withId(R.id.age_edit_text))
+            .check(matches(withHint(R.string.enter_age)))
+    }
+
+    @Test
+    fun canEnterText() {
+        onView(withId(R.id.name_edit_text))
+            .perform(typeText("Bob Doe"), closeSoftKeyboard())
+            .check(matches(withText("Bob Doe")))
+
+        onView(withId(R.id.email_edit_text))
+            .perform(typeText("bdoe@gmail.com"), closeSoftKeyboard())
+            .check(matches(withText("bdoe@gmail.com")))
+
+        onView(withId(R.id.username_edit_text))
+            .perform(typeText("bdoe"), closeSoftKeyboard())
+            .check(matches(withText("bdoe")))
+
+        onView(withId(R.id.age_edit_text))
+            .perform(typeText("25"), closeSoftKeyboard())
+            .check(matches(withText("25")))
+    }
+
+    @Test
+    fun canEnterDate() {
+
+        fun withDate(year: Int, month: Int, day: Int): Matcher<View> {
+            return object: TypeSafeMatcher<View>() {
+                override fun describeTo(description: Description?) {
+                    description?.appendText("matches date:")
+                }
+
+                override fun matchesSafely(item: View): Boolean {
+                    println("")
+                    return if (item is DatePicker) {
+                        val matchesYear = year == (item as DatePicker).year
+                        val matchesMonth = month == (item as DatePicker).month + 1
+                        val matchesDay = day == (item as DatePicker).dayOfMonth
+                        matchesYear && matchesMonth && matchesDay
+                    } else return false
+                }
+            }
+        }
+
+        onView(withId(R.id.date_of_birth))
+            .perform(PickerActions.setDate(2010, 12, 5))
+            .check(matches(withDate(2010, 12, 5)))
     }
 }
--- app/src/main/java/com/ad340/datingapp/MainActivity.kt
+++ app/src/main/java/com/ad340/datingapp/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.ad340.datingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToProfileActivity(view: View) {
+        val intent = Intent(this, ProfileActivity::class.java)
+        val bundle = Bundle()
+
+        val name = findViewById<EditText>(R.id.name_edit_text).text.toString()
+        bundle.putString(Constants.KEY_NAME, name)
+
+        val email = findViewById<EditText>(R.id.email_edit_text).text.toString()
+        bundle.putString(Constants.KEY_EMAIL, email)
+
+        val username = findViewById<EditText>(R.id.username_edit_text).text.toString()
+        bundle.putString(Constants.KEY_USERNAME, username)
+
+        val age = findViewById<EditText>(R.id.age_edit_text).text.toString()
+        bundle.putString(Constants.KEY_AGE, age)
+
+        val dateOfBirth = findViewById<DatePicker>(R.id.date_of_birth)
+        val dateOfBirthArr = intArrayOf(
+            dateOfBirth.year,
+            dateOfBirth.month + 1,
+            dateOfBirth.dayOfMonth
+        )
+        bundle.putIntArray(Constants.KEY_DOB, dateOfBirthArr)
+
+        startActivity(intent)
     }
 }
